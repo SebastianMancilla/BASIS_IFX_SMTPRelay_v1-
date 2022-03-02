5983b5a9b69dfcd3f24ec12137c01712cfbdac19
SebastianMancilla <91504836+SebastianMancilla@users.noreply.github.com>
2022-03-02 11:18:35 -0600

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net.Mail
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Mail As New MailMessage
+        Mail.Subject = "SMTP Relay prueba"
+        Mail.To.Add("ncastaneda@basiscons.com")
+        Mail.From = New MailAddress("info@sebastianmancilla.online")
+        Mail.Body = "Hola Mundo :)"
+        Mail.IsBodyHtml = False
+
+        Dim SMTP As New SmtpClient("smtp.hostinger.com")
+        SMTP.EnableSsl = True
+        SMTP.Credentials = New System.Net.NetworkCredential("info@sebastianmancilla.online", "Taller123")
+        SMTP.Port = 587
+        SMTP.Send(Mail)
+        MsgBox("Mensaje Enviado")
+    End Sub
+End Class
